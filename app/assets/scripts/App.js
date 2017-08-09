314ec8f0e5648ce13fc68844238cb6ca6e97b1a8
Alfaiz <alfaizmomin@gmail.com>
2017-08-09 13:01:21 +0530

    optimizing speed 1.1

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,6 @@ import MobileMenu from './modules/MobileMenu';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
 
-
-
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
 
